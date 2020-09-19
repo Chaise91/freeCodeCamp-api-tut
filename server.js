2a21315ee50a0f3e9dac9c8f559870bff0ee4b90
BuildTools <unconfigured@null.spigotmc.org>
2020-09-18 21:57:19 -0400

    Converted to ES6?

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express")
-const MongoClient = require("mongodb").MongoClient
-const bodyParser = require("body-parser")
-const app = express()
-const db = require("./db")
-const port = 8000
+import express from "express";
+import { MongoClient } from "mongodb";
+import { urlencoded } from "body-parser";
+const app = express();
+import { url } from "./db";
+const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, db) => {
-  if (err) return console.log(err)
-  db = db.db("such-notes")
-  require("./routes")(app, db)
+MongoClient.connect(url, (err, db) => {
+  if (err) return console.log(err);
+  db = db.db("such-notes");
+  require("./routes")(app, db);
   app.listen(port, () => {
-    console.log("we are live on " + port)
-  })
-})
+    console.log("we are live on " + port);
+  });
+});
 
 /* Adding a comment to test git */
